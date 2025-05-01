3e5c5509ad1796376444965d43a2457a37373ed8
KasaharaSota <souta.programer1201@gmail.com>
2025-05-01 22:04:55 +0900

    [update]移動に関する入力の受付

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance;
     PlayerPiece _playerPiece;
+    PlayerInput _playerInput;
+    Vector2 _moveInput;
+    public Vector2 MoveInput => _moveInput;
     void Start()
     {
+        Instance = this;
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.actions["Move"].performed += OnMove;
+        _playerInput.actions["Move"].canceled += StopMove;
+    }
+    void OnMove(InputAction.CallbackContext callbackContext)
+    {
+        _moveInput = callbackContext.ReadValue<Vector2>();
+    }
+    void StopMove(InputAction.CallbackContext callbackContext)
+    {
+        _moveInput = Vector2.zero;
     }
     void Update()
     {
-        
     }
     public void SetPlayerPiece(PlayerPiece playerPiece)
     {
