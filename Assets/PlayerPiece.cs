39dadcda7f0f1ace0517ac9b61368f85647d55a7
KasaharaSota <souta.programer1201@gmail.com>
2025-05-02 17:19:37 +0900

    [update]床にプレイヤーが乗ったことを検知させる

--- Assets/PlayerPiece.cs
+++ Assets/PlayerPiece.cs
@@ -7,6 +7,7 @@ public class PlayerPiece : MonoBehaviour
 {
     [SerializeField] float _time = 1;
     Queue<Direction> _commands;
+    bool _isMoving;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,41 +24,58 @@ public class PlayerPiece : MonoBehaviour
     {
         _commands.Enqueue(direction);
     }
+    public void Fall()
+    {
+        if (!_isMoving)
+        {
+            _commands.Clear();
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb = transform.GetChild(0).GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            VirtualCameraManager.Instance.ChangeFollow(null);
+            SoundManager.Instance.PlaySE("Fall");
+        }
+    }
     IEnumerator Move()
     {
-        while(true)
+        yield return null;
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit))
+        {
+            hit.collider.GetComponent<Floor>().FadeDisAppear();
+        }
+        while (true)
         {
             if(_commands.Count > 0)
             {
                 Direction dir = _commands.Dequeue();
-                bool isMoving = true;
+                _isMoving = true;
                 switch (dir)
                 {
                     case Direction.Up:
                         transform.forward = Vector3.forward;
-                        transform.DOMoveZ(transform.position.z + 1, _time).SetEase(Ease.Linear).OnComplete(() => isMoving = false); break;
+                        transform.DOMoveZ(transform.position.z + 1, _time).SetEase(Ease.Linear).OnComplete(() => _isMoving = false); break;
                     case Direction.Down:
                         transform.forward = Vector3.back;
-                        transform.DOMoveZ(transform.position.z - 1, _time).SetEase(Ease.Linear).OnComplete(() => isMoving = false); break;
+                        transform.DOMoveZ(transform.position.z - 1, _time).SetEase(Ease.Linear).OnComplete(() => _isMoving = false); break;
                     case Direction.Right:
                         transform.forward = Vector3.right;
-                        transform.DOMoveX(transform.position.x + 1, _time).SetEase(Ease.Linear).OnComplete(() => isMoving = false); break;
+                        transform.DOMoveX(transform.position.x + 1, _time).SetEase(Ease.Linear).OnComplete(() => _isMoving = false); break;
                     case Direction.Left:
                         transform.forward = Vector3.left;
-                        transform.DOMoveX(transform.position.x - 1, _time).SetEase(Ease.Linear).OnComplete(() => isMoving = false); break;
+                        transform.DOMoveX(transform.position.x - 1, _time).SetEase(Ease.Linear).OnComplete(() => _isMoving = false); break;
                 }
-                while(isMoving)
+                while(_isMoving)
                 {
                     yield return null;
                 }
-                if(!Physics.Raycast(transform.position,Vector3.down))
+                if(!Physics.Raycast(transform.position,Vector3.down,out hit))
                 {
-                    _commands.Clear();
-                    Rigidbody rb = GetComponent<Rigidbody>();
-                    rb.useGravity = true;
-                    rb = transform.GetChild(0).GetComponent<Rigidbody>();
-                    rb.useGravity = true;
-                    SoundManager.Instance.PlaySE("Fall");
+                    Fall();
+                }
+                else
+                {
+                    hit.collider.GetComponent<Floor>().FadeDisAppear();
                 }
             }
             else
